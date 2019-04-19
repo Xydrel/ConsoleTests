e0381f524c8d519edeb84a3532905ca989893e2b
Xydrel <jasonh78@gmail.com>
2019-04-19 16:27:25 -0700

    ADD: New depthfirst and breadth first visit methods ADD: New tests

--- TestConsole/BinarySearchTree.h
+++ TestConsole/BinarySearchTree.h
@@ -6,6 +6,8 @@
 
 void RunBinarySearchTreeTests()
 {
+	std::cout << "Testing tree balanced method functionality" << std::endl;
+
 	auto root = new BinaryTreeNode<int>(5);
 	root->InsertLeft(8)->InsertLeft(1);
 	root->InsertRight(6)->InsertRight(4);
@@ -46,4 +48,47 @@ void RunBinarySearchTreeTests()
 	root = new BinaryTreeNode<int>(1);
 	root->InsertRight(2)->InsertRight(3)->InsertRight(4);
 	std::cout << "IsBalanced(root): expected = true: " << (root->IsBalanced(root) ? "true" : "false") << std::endl;
+
+
+	std::cout << "Testing binary tree test function" << std::endl;
+
+	root = new BinaryTreeNode<int>(50);
+	root->InsertLeft(30)->InsertLeft(10);
+	root->InsertRight(70)->InsertRight(80);
+	root->GetLeftChildNode()->InsertRight(40);
+	root->GetRightChildNode()->InsertLeft(60);
+	std::cout << "IsBinarySearchTree(root): expected = true: " << (root->IsBinarySearchTree(root) ? "true" : "false") << std::endl;
+
+	root = new BinaryTreeNode<int>(50);
+	root->InsertLeft(30)->InsertLeft(20);
+	root->InsertRight(80)->InsertRight(90);
+	root->GetLeftChildNode()->InsertRight(60);
+	root->GetRightChildNode()->InsertLeft(70);
+	std::cout << "IsBinarySearchTree(root): expected = false: " << (root->IsBinarySearchTree(root) ? "true" : "false") << std::endl;
+
+	root = new BinaryTreeNode<int>(50);
+	root->InsertLeft(40)->InsertLeft(30)->InsertLeft(20)->InsertLeft(10);
+	std::cout << "IsBinarySearchTree(root): expected = true: " << (root->IsBinarySearchTree(root) ? "true" : "false") << std::endl;
+
+	root = new BinaryTreeNode<int>(50);
+	root->InsertRight(70)->InsertRight(60)->InsertRight(80);
+	std::cout << "IsBinarySearchTree(root): expected = false: " << (root->IsBinarySearchTree(root) ? "true" : "false") << std::endl;
+
+	root = new BinaryTreeNode<int>(50);
+	std::cout << "IsBinarySearchTree(root): expected = true: " << (root->IsBinarySearchTree(root) ? "true" : "false") << std::endl;
+
+
+	auto stringRoot = new BinaryTreeNode<std::string>("A");
+	stringRoot->InsertLeft("B")->InsertLeft("C");
+	stringRoot->GetLeftChildNode()->InsertRight("D");
+	stringRoot->InsertRight("E")->InsertLeft("F");
+	stringRoot->GetRightChildNode()->InsertRight("G");
+
+	std::cout << "Dept first visit: " << std::endl;
+	stringRoot->DepthFirstRecursive(stringRoot);
+
+	std::cout << "Breadth first visit " << std::endl;
+	stringRoot->BreadthFirstRecursive(stringRoot);
+
+
 }
--- TestConsole/BinaryTreeNode.h
+++ TestConsole/BinaryTreeNode.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <utility>
+#include <limits>
+#include <algorithm>
 #include <stack>
+#include <queue>
 #include <vector>
 
 template<typename T>
@@ -41,13 +44,14 @@ public:
 
 
 
-	bool HasLeftChildNode() { return (nullptr != this->mLeftNode); }
-	bool HasRightChildNode() { return (nullptr != this->mRightNode); }
+	bool HasLeftChildNode() const { return (nullptr != this->mLeftNode); }
+	bool HasRightChildNode() const { return (nullptr != this->mRightNode); }
 	bool HasData() { return (nullptr != this->mData); }
 
-	const T* GetData() { return mData; }
-	const BinaryTreeNode* GetLeftChildNodeConst() { return this->mLeftNode; }
-	const BinaryTreeNode* GetRightChildNodeConst() { return this->mRightNode; }
+	const T GetDataConst() { return mData; }
+	T GetData() const { return mData; }
+	const BinaryTreeNode* GetLeftChildNodeConst() const { return this->mLeftNode; }
+	const BinaryTreeNode* GetRightChildNodeConst() const { return this->mRightNode; }
 
 	BinaryTreeNode* GetLeftChildNode() { return this->mLeftNode; }
 	BinaryTreeNode* GetRightChildNode() { return this->mRightNode; }
@@ -56,12 +60,40 @@ public:
 	BinaryTreeNode* InsertRight(T data) { this->mRightNode = new BinaryTreeNode(data); return this->mRightNode; }
 
 	bool IsBalanced(const BinaryTreeNode* treeRoot);
+	bool IsBinarySearchTree(const BinaryTreeNode* treeRoot);
+
+	void BreadthFirst(const BinaryTreeNode<T>* root);
+	void BreadthFirstRecursive(const BinaryTreeNode<T>* root);
+	void DepthFirst(const BinaryTreeNode<T>* root);
+	void DepthFirstRecursive(const BinaryTreeNode<T>* root);
 
 private:
 	// Data could be anything so making this a pointer
 	T mData = NULL;
 	BinaryTreeNode* mLeftNode = nullptr;
 	BinaryTreeNode* mRightNode = nullptr;
+};
+
+class NodeBounds
+{
+public:
+	NodeBounds(const BinaryTreeNode<int>* inNode = nullptr,
+				int inLowerBound = std::numeric_limits<int>::min(),
+				int inUpperBound = std::numeric_limits<int>::max()) :
+		mNode(inNode),
+		mLowerBound(inLowerBound),
+		mUpperBound(inUpperBound)
+	{
+	}
+
+	BinaryTreeNode<int>* GetNode() { return (BinaryTreeNode<int>*)mNode; }
+	const int GetLowerBound() const { return mLowerBound; }
+	const int GetUpperBound() const { return mUpperBound; }
+
+private:
+	const BinaryTreeNode<int>* mNode;
+	int mLowerBound;
+	int mUpperBound;
 };
 
 template<typename T>
@@ -100,7 +132,7 @@ bool BinaryTreeNode<T>::IsBalanced(const BinaryTreeNode<T>* treeRoot)
 			}
 
 			if (depthCount > 2 ||
-				(depthCount == 2 && max(depths[0], depths[1]) - min(depths[0], depths[1]) > 1))
+			   (depthCount == 2 && max(depths[0], depths[1]) - min(depths[0], depths[1]) > 1))
 			{
 				return false;
 			}
@@ -121,3 +153,135 @@ bool BinaryTreeNode<T>::IsBalanced(const BinaryTreeNode<T>* treeRoot)
 	
 	return true;
 }
+
+// Testing for if Binary Tree by performing depth-first search on nodes
+template<typename T>
+bool BinaryTreeNode<T>::IsBinarySearchTree(const BinaryTreeNode<T>* treeRoot)
+{
+	if (treeRoot == nullptr)
+	{
+		return true;
+	}
+
+	std::stack<NodeBounds> nodeAndBoundsStack;
+	nodeAndBoundsStack.push(NodeBounds(treeRoot));
+
+	while (!nodeAndBoundsStack.empty())
+	{
+		NodeBounds nodeBounds = nodeAndBoundsStack.top();
+		nodeAndBoundsStack.pop();
+
+		const int nodeValue = nodeBounds.GetNode()->GetData();
+		if (nodeValue <= nodeBounds.GetLowerBound() || nodeValue >= nodeBounds.GetUpperBound())
+		{
+			return false;
+		}
+
+		if (nodeBounds.GetNode()->GetLeftChildNode() != nullptr)
+		{
+			nodeAndBoundsStack.push(NodeBounds(nodeBounds.GetNode()->GetLeftChildNode(), 
+									nodeBounds.GetLowerBound(), nodeValue));
+		}
+
+		if (nodeBounds.GetNode()->GetRightChildNode() != nullptr)
+		{
+			nodeAndBoundsStack.push(NodeBounds(nodeBounds.GetNode()->GetRightChildNode(),
+									nodeValue, nodeBounds.GetUpperBound()));
+		}
+	}
+
+	return true;
+}
+
+// iterative breadthfirst, not ordering correctly - revisit
+template<typename T>
+void BinaryTreeNode<T>::BreadthFirst(const BinaryTreeNode<T>* root)
+{
+	if (root == nullptr)
+	{
+		return;
+	}
+
+	queue<const BinaryTreeNode<T>*> nodeQueue;
+	nodeQueue.push(root);
+
+	while (!nodeQueue.empty())
+	{
+		const BinaryTreeNode* node = nodeQueue.front();
+		nodeQueue.pop();
+
+		std::cout << "Node Data: " << (node->GetData()) << std::endl;
+
+		if (node->HasLeftChildNode())
+		{
+			nodeQueue.push(node->GetLeftChildNodeConst());
+		}
+
+		if (node->HasRightChildNode())
+		{
+			nodeQueue.push(node->GetRightChildNodeConst());
+		}
+	}
+}
+
+// ordering not correct - revisit
+template<typename T>
+inline void BinaryTreeNode<T>::BreadthFirstRecursive(const BinaryTreeNode<T>* root)
+{
+	if (root == nullptr)
+	{
+		return;
+	}
+
+	std::cout << "Node Data: " << (root->GetData()) << std::endl;
+
+	BreadthFirstRecursive(root->GetLeftChildNodeConst());
+	BreadthFirstRecursive(root->GetRightChildNodeConst());
+}
+
+// iterative depthfirst, not ordering correctly - revisit
+template<typename T>
+void BinaryTreeNode<T>::DepthFirst(const BinaryTreeNode<T>* root)
+{
+	if (root == nullptr)
+	{
+		return;
+	}
+
+	stack<const BinaryTreeNode<T>*> nodeStack;
+	nodeStack.push(root);
+
+	while (!nodeStack.empty())
+	{
+		const BinaryTreeNode* node = nodeStack.top();
+		nodeStack.pop();
+
+		std::cout << "Node Data: " << (node->GetData()) << std::endl;
+
+		if (node->HasLeftChildNode())
+		{
+			nodeStack.push(node->GetLeftChildNodeConst());
+		}
+
+		if (node->HasRightChildNode())
+		{
+			nodeStack.push(node->GetRightChildNodeConst());
+		}
+	}
+}
+
+// ordering as expected
+template<typename T>
+inline void BinaryTreeNode<T>::DepthFirstRecursive(const BinaryTreeNode<T>* root)
+{
+	if (root == nullptr)
+	{
+		return;
+	}
+	
+	std::cout << "Node Data: " << (root->GetData()) << std::endl;
+	DepthFirstRecursive(root->GetLeftChildNodeConst());
+	DepthFirstRecursive(root->GetRightChildNodeConst());
+}
+
+
